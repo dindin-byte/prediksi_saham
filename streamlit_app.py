e52cf906144faa0c51043f952538e1b1c8f30832
dindin-byte <74443172+dindin-byte@users.noreply.github.com>
2023-10-22 11:22:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,25 +9,25 @@ st.title('Aplikasi Prediksi Saham')
 st.subheader('Data Saham')
 st.write(df)
 
-# Menampilkan grafik prediksi
-plt.figure(figsize=(16, 6))
+# Membuat grafik prediksi
 for stock, data in data_dict.items():
     last_date = data.index[-1]
     future_dates = pd.date_range(start=last_date, periods=len(future_predictions[stock]) + 1, freq='B')[1:]
     future_data = pd.Series(future_predictions[stock], index=future_dates)
-    plt.plot(data.index, data['Close'], label=f'{stock} Actual')
-    plt.plot(future_data.index, future_data, label=f'{stock} Future Prediction')
+    fig, ax = plt.subplots()
+    ax.plot(data.index, data['Close'], label=f'{stock} Actual')
+    ax.plot(future_data.index, future_data, label=f'{stock} Future Prediction')
     
     if future_data[-1] > future_data[-2]:
-        plt.annotate('↑', (future_data.index[-1], future_data.values[-1]), color='g', fontsize=30, ha='center', va='center')
+        ax.annotate('↑', (future_data.index[-1], future_data.values[-1]), color='g', fontsize=30, ha='center', va='center')
     else:
-        plt.annotate('↓', (future_data.index[-1], future_data.values[-1]), color='r', fontsize=30, ha='center', va='center')
+        ax.annotate('↓', (future_data.index[-1], future_data.values[-1]), color='r', fontsize=30, ha='center', va='center')
 
-    plt.title('Predictions for the Future')
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.legend()
-    st.pyplot(plt)
+    ax.set_title('Predictions for the Future')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Price')
+    ax.legend()
+    st.pyplot(fig)
 
 # Menampilkan data aktual dan prediksi
 st.subheader('Data Aktual dan Prediksi')
